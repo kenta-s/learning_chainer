775544c4ccac21bf82d99025eabb823a5016f058
kenta-s <knt01222@gmail.com>
2017-06-06 08:22:58 +0900

    default.py

---
+++ default.py
@@ -0,0 +1,5 @@
+import chainer
+from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
