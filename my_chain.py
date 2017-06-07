7a708f8ab7c4ca5d83b382aa400ea7df326431b2
kenta-s <knt01222@gmail.com>
2017-06-07 13:12:43 +0900

    Add some instance methods to MyChain

--- my_chain.py
+++ my_chain.py
@@ -1,3 +1,12 @@
 class MyChain(Chain):
     def __init__(self):
-        return self
+        super(MyChain, self).__init__(
+            l1 = L.Linear(4, 3),
+            l2 = L.Linear(3, 3),
+        )
+
+    def __call__(self, x, y):
+        return true
+
+    def fwd(self, x, y):
+        return F.sigmoid(l1(x))
