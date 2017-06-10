cf3f922ceb720fa2cdcac16d811948dc3fa8f8fc
kenta-s <knt01222@gmail.com>
2017-06-10 09:26:05 +0900

    Logistic regression

---
+++ iris/logistic.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append('../')
+from default import *
+from sklearn import datasets
+
+class IrisLogistic(Chain):
+    def __init__(self):
+        super(IrisLogistic, self).__init__(
+            l1=L.Linear(4, 3),
+        )
+
+    def __call__(self, x, y):
+        return F.mean_squared_error(self.fwd(x), y)
+
+    def fwd(self, x):
+        return F.softmax(self.l1(x))
+
+iris = datasets.load_iris()
+X = iris.data.astype(np.float32)
+Y = iris.target
+N = Y.size
+Y2 = np.zeros(3 * N).reshape(N, 3).astype(np.float32)
+for i in range(N):
+    Y2[i, Y[i]] = 1.0
+
+index = np.arange(N)
+xtrain = X[index[index % 2 != 0],:]
+ytrain = Y2[index[index % 2 != 0],:]
+xtest = X[index[index % 2 == 0],:]
+yans = Y[index[index % 2 == 0]]
+
+model = IrisLogistic()
+optimizer = optimizers.Adam()
+optimizer.setup(model)
+
+for i in range(10000):
+    x = Variable(xtrain)
+    y = Variable(ytrain)
+    model.zerograds()
+    loss = model(x, y)
+    loss.backward()
+    optimizer.update()
+
+xt = Variable(xtest)
+yt = model.fwd(xt)
+ans = yt.data
+nrow, ncol = ans.shape
+ok = 0
+for i in range(nrow):
+    cls = np.argmax(ans[i,:])
+    if cls == yans[i]:
+        ok += 1
+
+print(ok, "/", nrow, " = ", (ok * 1.0)/nrow)
