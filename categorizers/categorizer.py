4e8b2d87138a7fc81e5ff655a1d1868cbeda274e
kenta-s <knt01222@gmail.com>
2017-06-12 09:53:46 +0900

    Make MeCab works

--- categorizers/categorizer.py
+++ categorizers/categorizer.py
@@ -1,10 +1,33 @@
 # You must install mecab before running this script.
 
-# brew install mecab
-# brew install mecab-ipadic
-# pip install mecab-python3
+# $ brew install mecab
+# $ brew install mecab-ipadic
+# $ pip install mecab-python3
+
+# And gensim
+# $ pip install gensim
 
 import MeCab
+from gensim import corpora
+
 mecab = MeCab.Tagger("-Ochasen")
 
-text = mecab.parse("これはすごいテキストです")
+mecab.parse('') # まずいタイミングでtextがGCされるらしくてmecabがエラー吐く問題を対処するハック。これは酷い。
+
+text = "【ナダル快挙 感動表現できない】「赤土の王者」ナダルが全仏10度目のV。「今は感動ばかりで言葉が見つかりません」と優勝杯を力強く抱きかかえ、会心の笑顔。"
+node = mecab.parseToNode(text)
+
+words = []
+while node:
+    meta = node.feature.split(",")
+    if meta[0] == "名詞":
+        words.append(node.surface)
+    node = node.next
+
+dictionary = corpora.Dictionary([words])
+print(dictionary.token2id)
+
+# from IPython import embed
+# from IPython.terminal.embed import InteractiveShellEmbed
+#
+# embed()
